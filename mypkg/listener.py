110b6a47ca7357a056cfd8a9d9f1cf3939b8c472
Kenta Tsukino <s21c1082ns@s.chibakoudai.jp>
2023-01-05 21:37:58 +0900

    fix listener

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,24 +1,15 @@
 # SPDX-FileCopyrightText: 2022 KentaTsukino
 # SPDX-License-Identifier: BSD-3-Clause
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-class Listener():
-    def __init__(self, node):
-        self.pub = node.create_subscription(Int16, "countup", self.cb, 10)
-        self.node = node
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
 
-    def cb(self, msg):
-        self.node.get_logger().info("Listen: %d" % msg.data)
-
-def main():
-    rclpy.init()
-    node = Node("listener")
-    listener = Listener(node)
-    rclpy.spin(node)
-
-if __name__ == '__main__':
-    main()
+rclpy.spin(node)
