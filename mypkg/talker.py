54b25b2bb43a0a45c05b7689eebf302e5726f6cc
Kenta Tsukino <s21c1082ns@s.chibakoudai.jp>
2023-01-05 17:01:37 +0900

    pudate class

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2022 KentaTsukino 　　　　　
 # SPDX-License-Identifier: BSD-3-Clause
 
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
@@ -10,16 +9,19 @@ class Talker():
   def __init__(self, node):
     self.pub = node.create_publisher(Int16, "countup", 10)
     self.n = 0
+    node.create_timer(0.5, self.cb)
 
-rclpy.init()
-node = Node("talker")
-talker = Talker()
-def cb():
+  def cb(self):
     msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
+    msg.data = self.n
+    self.pub.publish(msg)
+    self.n += 1
 
-node.create_timer(0.5, self.cb)
-rclpy.spin(node)
+def main():
+  rclpy.init()
+  node = Node("talker")
+  talker = Talker(node)
+  rclpy.spin(node)
 
+if __name__ == '__main__':
+  main()
